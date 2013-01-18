e421f524dc063a7f8e8253093c6e2bdc9bc41dda
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2013-01-18 02:08:49 +0200

    The very first commit. Is the eclipse push to repo working ?

---
+++ DynamoDB.cpp
@@ -0,0 +1,21 @@
+/*
+ * DynamoDB.cpp
+ *
+ *  Created on: Jan 18, 2013
+ *      Author: enigma
+ */
+
+#include "DynamoDB.h"
+
+namespace AWS {
+
+DynamoDB::DynamoDB() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DynamoDB::~DynamoDB() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace AWS */
---
+++ DynamoDB.h
@@ -0,0 +1,20 @@
+/*
+ * DynamoDB.h
+ *
+ *  Created on: Jan 18, 2013
+ *      Author: enigma
+ */
+
+#ifndef DYNAMODB_H_
+#define DYNAMODB_H_
+
+namespace AWS {
+
+class DynamoDB {
+public:
+	DynamoDB();
+	virtual ~DynamoDB();
+};
+
+} /* namespace AWS */
+#endif /* DYNAMODB_H_ */
